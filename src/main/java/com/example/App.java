290f4153e8a5e5fba52e6be7a9e376ab2ecce91e
bitcse02 <bitcsedevops02@gmail.com>
2025-06-06 15:03:54 +0530

    intial

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hi i am pranav from b section with usn:1bi22cs112" );
+    }
+}
